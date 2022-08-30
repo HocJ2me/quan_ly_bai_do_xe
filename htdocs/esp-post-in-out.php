35f1faa858f1b1bdc2dc1d66e00696cd0a8b12ff
Lê Chí Tuyền <45262669+HocJ2me@users.noreply.github.com>
2022-08-31 04:58:52 +0700

    sửa file server
    chỉnh lại code server

--- htdocs/esp-post-in-out.php
+++ htdocs/esp-post-in-out.php
@@ -7,12 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 	$today = date('Y-m-d H:i:s');                 // March 10, 2001, 5:16 pm
 
 	$rfid = $_GET["rfid"];
-	$temperature_1 = $_GET["t1"];
-	$temperature_2 = $_GET["t2"];
-	$temperature_3 = $_GET["t3"];
-	$temperature_4 = $_GET["t4"];
-	$temperature_5 = $_GET["t5"];
-	
 	
 	// Kết nối CSDL
 	$conn = mysqli_connect('localhost', 'root', '', 'bai-do-xe-db');
@@ -46,25 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 		echo $sql;
 		$result = mysqli_query($conn, $sql);
 		
-		if ($result)
-		{
-			
-			$sql = "UPDATE `thong_tin_diem_do` SET `nhiet_do`='".$temperature_1."' WHERE `id`='1'";
-			$result = mysqli_query($conn, $sql);
-			$sql = "UPDATE `thong_tin_diem_do` SET `nhiet_do`='".$temperature_2."' WHERE `id`='2'";
-			$result = mysqli_query($conn, $sql);
-			$sql = "UPDATE `thong_tin_diem_do` SET `nhiet_do`='".$temperature_3."' WHERE `id`='3'";
-			$result = mysqli_query($conn, $sql);
-			$sql = "UPDATE `thong_tin_diem_do` SET `nhiet_do`='".$temperature_4."' WHERE `id`='4'";
-			$result = mysqli_query($conn, $sql);
-			$sql = "UPDATE `thong_tin_diem_do` SET `nhiet_do`='".$temperature_5."' WHERE `id`='5'";
-			$result = mysqli_query($conn, $sql);
-			// ngắt kết nối
-			mysqli_close($conn);
-		}else
-		{
-			echo "Update ERROR";
-		}
+	
 		
 	}else
 	{
